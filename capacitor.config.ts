d8826d67d8f59b0fbb4cc53d7feab8534182e6d4
Shishir Umrao <shishirumrao42@gmail.com>
2023-12-02 22:55:44 +0530

    project setup after cleanup

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ofity Operations Portal',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
